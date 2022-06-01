9d09f75bce713456c6f7104b02c3a74d67a7f75c
Mark Nelson <mark.x.nelson@oracle.com>
2022-06-01 12:55:33 -0400

    teweak script
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- src/create_topic.sql
+++ src/create_topic.sql
@@ -3,9 +3,9 @@ create or replace procedure create_topic (
     topic_name in varchar2) as
 begin
     dbms_aqadm.create_transactional_event_queue(
-        queue_name => qname,
+        queue_name => topic_name,
         multiple_consumers => true
     );
-    dbms_aqadm.start_queue(qname);
+    dbms_aqadm.start_queue(topic_name);
 end;
 /
